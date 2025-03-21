0485c361163897d56761dc6d9c627959ead3a1f7
DarkWister <darkwistercrak17@gmail.com>
2025-03-21 01:18:39 +0000

    init

---
+++ android/app/src/main/java/ionic/multas20230188/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.multas20230188;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.multas20230188',
+  appName: 'multas20230188',
+  webDir: 'www'
+};
+
+export default config;
