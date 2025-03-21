f37c0437dc90b4d3953972a69230fb8d3ca119f4
DarkWister <darkwistercrak17@gmail.com>
2025-03-21 23:17:59 +0000

    working View Module and final version of service

--- src/app/services/multa.service.ts
+++ src/app/services/multa.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 import { Multa } from '../classes/multa';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MultaService {
   private _storage: Storage | null = null;
+  private multasSubject: BehaviorSubject<Multa[]> = new BehaviorSubject<Multa[]>([]);
 
   constructor(private storage: Storage) {
     this.init();
   }
 
-  async init() {
+  async init(): Promise<void> {
     this._storage = await this.storage.create();
+    await this.loadMultas();
+  }
+
+  async loadMultas() {
+    const multas = await this.obtenerMultas();
+    this.multasSubject.next(multas);
+  }
+
+  get multas$() {
+    return this.multasSubject.asObservable();
   }
 
   async guardarMulta(multa: Multa) {
@@ -21,6 +33,7 @@ export class MultaService {
     multa.id = multas.length > 0 ? multas[multas.length - 1].id! + 1 : 1;
     multas.push(multa);
     await this._storage?.set('multas', multas);
+    this.multasSubject.next(multas);  // Emitir la lista actualizada
   }
 
   async obtenerMultas(): Promise<Multa[]> {
@@ -36,5 +49,6 @@ export class MultaService {
     const multas = await this.obtenerMultas();
     const nuevasMultas = multas.filter(multa => multa.id !== id);
     await this._storage?.set('multas', nuevasMultas);
+    this.multasSubject.next(nuevasMultas);
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,45 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import { MultaService } from '../services/multa.service';
+import { Multa } from '../classes/multa';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonThumbnail, IonImg, IonItemSliding, IonItemOptions, IonItemOption } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonThumbnail, IonImg, IonItemSliding, IonItemOptions, IonItemOption, CommonModule
+]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  multas: Multa[] = [];
+  private multasSubscription?: Subscription;
 
-  constructor() {}
+  constructor(private multaService: MultaService, private router: Router) {}
 
+  ngOnInit() {
+    this.multasSubscription = this.multaService.multas$.subscribe(multas => {
+      this.multas = multas;
+    });
+
+    // Cargar las multas al inicio
+    this.multaService.loadMultas();
+  }
+
+  ngOnDestroy() {
+    if (this.multasSubscription) {
+      this.multasSubscription.unsubscribe();
+    }
+  }
+
+  verDetalles(id: number) {
+    this.router.navigate([`/detalle-multa/${id}`]);
+  }
+
+  async eliminarMulta(id: number) {
+    await this.multaService.eliminarMulta(id);
+  }
 }
