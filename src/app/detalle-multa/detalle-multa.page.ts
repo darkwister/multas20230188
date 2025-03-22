df05094b77144dcc6af3b41786cb9e3a62e5e1fa
DarkWister <darkwistercrak17@gmail.com>
2025-03-22 00:13:04 +0000

    Detalle works, about me module done, myself image added

--- src/app/detalle-multa/detalle-multa.page.ts
+++ src/app/detalle-multa/detalle-multa.page.ts
@@ -1,20 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardSubtitle, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonImg, IonText, IonButton, IonButtons, IonBackButton, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { Multa } from '../classes/multa';
+import { ActivatedRoute } from '@angular/router';
+import { MultaService } from '../services/multa.service';
+import { VoiceRecorder } from 'capacitor-voice-recorder';
+import L from 'leaflet';
 
 @Component({
   selector: 'app-detalle-multa',
   templateUrl: './detalle-multa.page.html',
   styleUrls: ['./detalle-multa.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonLabel, IonItem, IonBackButton, IonButtons, IonButton, IonText, IonImg, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCardSubtitle, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class DetalleMultaPage implements OnInit {
 
-  constructor() { }
+  multa?: Multa;
+  public map!: L.Map;
 
-  ngOnInit() {
+  constructor(private multaService: MultaService, private router: ActivatedRoute) { }
+
+  async ngOnInit() {
+    if (!this.multa) {
+      const id = this.router.snapshot.paramMap.get('id');
+      if (id) {
+        this.multa = await this.multaService.obtenerMultaPorId(parseInt(id));
+        console.log(this.multa);
+        this.loadMap();
+      }
+    }
   }
-
+  loadMap() {
+    if (!this.multa?.ubicacion) return;
+  
+    const [lat, lng] = this.multa.ubicacion.split(',').map(Number);
+  
+    // Verificar si la instancia del mapa ya existe, si no, crearla
+    if (!this.map) {
+      this.map = L.map('map').setView([lat, lng], 15);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(this.map);
+    } else {
+      // Si el mapa ya existe, solo actualizamos la vista
+      this.map.setView([lat, lng], 15);
+    } 
+    const customIcon = L.icon({
+            iconUrl: 'assets/marker-icon.png', 
+            shadowUrl: 'assets/marker-shadow.png', 
+            iconSize: [25, 41],
+            iconAnchor: [12, 41], 
+            popupAnchor: [1, -34], 
+            shadowSize: [41, 41] 
+          });
+    L.marker([lat, lng], { icon: customIcon }).addTo(this.map)
+      .bindPopup('Ubicación de la multa')
+      .openPopup();
+  
+    // Asegurar que el mapa se redibuje correctamente
+    this.map.invalidateSize();
+  }
+  // Método para reproducir el audio
+  reproducirAudio(audioBase64: string) {
+    const audio = new Audio();
+    // Establecer la fuente del audio en base64
+    audio.src = audioBase64;
+    // Reproducir el audio
+    audio.play().catch((error) => {
+      console.error("Error al reproducir el audio: ", error);
+    });
+  }
 }
